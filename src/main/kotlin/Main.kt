ecd2db78b4d808110861848435f49bdb29899515
Halil Ozel <halilozel1903@gmail.com>
2023-11-05 17:50:11 +0300

    Less code added for Main.kt

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -5,22 +5,10 @@ check for prime numbers. In this case, we've set it to 20.
 
 fun main() {
     val limit = 20
-    print("Prime numbers up to $limit: ")
-    for (i in 2..limit) {
-        if (isPrime(i)) {
-            print("$i ")
-        }
-    }
+    println("Prime numbers up to $limit: " + (2..limit).filter { isPrime(it) }.joinToString(" "))
 }
 
 fun isPrime(number: Int): Boolean {
-    if (number <= 1) {
-        return false
-    }
-    for (i in 2 until number) {
-        if (number % i == 0) {
-            return false
-        }
-    }
-    return true
+    if (number <= 1) return false
+    return (2 until number).all { number % it != 0 }
 }
